2f6760a199fb5238e819919f8c5bc12938413f6e
YEJIN <yejon9858@gmail.com>
2022-02-02 05:00:33 +0900

    [INIT] 프로젝트 생성

---
+++ earthgarden/src/main/java/com/stopclimatechange/earthgarden/EarthgardenApplication.java
@@ -0,0 +1,13 @@
+package com.stopclimatechange.earthgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthgardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthgardenApplication.class, args);
+	}
+
+}
