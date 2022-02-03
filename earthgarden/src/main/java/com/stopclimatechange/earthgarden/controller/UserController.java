9daf74f32b219e970635dc8a440f62ea048aa447
YEJIN <yejon9858@gmail.com>
2022-02-04 03:43:29 +0900

    [FEAT] 회원가입, 로그인

---
+++ earthgarden/src/main/java/com/stopclimatechange/earthgarden/controller/UserController.java
@@ -0,0 +1,63 @@
+package com.stopclimatechange.earthgarden.controller;
+
+import com.stopclimatechange.earthgarden.config.JwtTokenProvider;
+import com.stopclimatechange.earthgarden.domain.User;
+import com.stopclimatechange.earthgarden.domain.UserDto;
+import com.stopclimatechange.earthgarden.repository.UserRepository;
+import com.stopclimatechange.earthgarden.service.MailService;
+import com.stopclimatechange.earthgarden.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collections;
+import java.util.HashMap;
+
+@Slf4j
+@CrossOrigin
+@Controller
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping(value = "/user/signup",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<HashMap> create(@RequestPart(value = "user") UserDto userDto, @RequestPart(value = "image", required = false) MultipartFile image) {
+
+        User user = userService.signUp(userDto, image);
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        responseMap.put("status", 200);
+        responseMap.put("message", "회원가입 성공");
+        return new ResponseEntity<HashMap>(responseMap, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/user/signin")
+    public ResponseEntity<HashMap> login(@RequestBody UserDto.LoginDto loginDto) {
+
+        User user = userService.signIn(loginDto);
+        String token = jwtTokenProvider.createToken(user.getEmail(), user.getRoles());
+        HashMap<String, Object> responseMap = new HashMap<>();
+        if (user != null) {
+            responseMap.put("status", 200);
+            responseMap.put("message", "로그인 성공");
+            responseMap.put("token", token);
+            //log.info(userRepository.findByEmail(jwtTokenProvider.getUserEmail(token)).orElseGet(() -> null ).getRoles().toString());
+            return new ResponseEntity<HashMap>(responseMap, HttpStatus.OK);
+        } else {
+            responseMap.put("status", 401);
+            responseMap.put("message", "이메일 또는 비밀번호 오류");
+            return new ResponseEntity<HashMap>(responseMap, HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
---
+++ earthgarden/src/main/java/com/stopclimatechange/earthgarden/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.stopclimatechange.earthgarden.service;
+
+import com.stopclimatechange.earthgarden.domain.User;
+import com.stopclimatechange.earthgarden.domain.UserDto;
+import com.stopclimatechange.earthgarden.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.util.Collections;
+
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;                              // 패스워드 인코더
+    private final ImageUploadService imageUploadService;
+
+    @Override
+    public User signUp(UserDto userDto, MultipartFile image) {
+        userDto.setRoles(Collections.singletonList("ROLE_USER"));
+        userDto.setPw(passwordEncoder.encode(userDto.getPw()));           // 비밀번호 암호화
+        userDto.setImage_url(imageUploadService.restore(image));
+        return userRepository.save(new User(userDto));
+    }
+
+    @Override
+    public User signIn(UserDto.LoginDto loginDto) {
+        User user = userRepository.findByEmail(loginDto.getEmail()).orElseGet(() -> null );             // 이메일로 user 검색
+
+        if(user==null)
+            return null;
+        else if (!passwordEncoder.matches(loginDto.getPw(), user.getPw()))           // 패스워드 확인
+            return null;
+
+        return user;
+    }
+
+    private Boolean validateDuplicateEmail(String email){
+        return userRepository.existsByEmail(email);
+    }
+
+    private Boolean validateDuplicateNickname(String nickname){
+        return userRepository.existsByNickname(nickname);
+    }
+
+}
